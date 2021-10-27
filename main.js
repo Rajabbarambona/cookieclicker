d438e47cf3351eaa2fab1195bcba36d97fa0f7bd
Poorani Karunanithi <Pooranikarunanithi09@gmail.com>
2021-10-27 18:22:47 +0200

    added images,title icon,animation for image

--- main.js
+++ main.js
@@ -1,29 +1,66 @@
-(() => {
+const labelCounter = document.getElementById("counter");
+const btnIncrease = document.getElementById("increase");
+const btnDecrease = document.getElementById("decrease");
+const btnReset = document.getElementById("reset");
+const btnMultiplier = document.getElementById("multi");
+const messageLabel = document.getElementById("message");
+const multiplierPriceLabel = document.getElementById("multiplierPrice");
 
-const emptyArray = [];
-const startbtn = document.getElementById("run");
-const counterInitializ = document.getElementById("image1.jpg");
-const label = document.getElementById("label");
-var count = -1;
+/initialize the conuter to zero/
+let count = 0;
+let multiplierValue = 0; // will keep the multiplier purchase value
 
-startbtn.addEventListener("click", () => {
-    count++;
-    console.log(count);
-    document.getElementById("zero").innerHTML = count;
+multiplierPriceLabel.innerHTML = count;
 
-    var count = 0;
-    var button = document.getElementById('initializ');
-    
-    button.onclick = updateStatus;
-    
-    function updateStatus() {
-       // button.textContent = 'You have ' + ++count + ' Cookies';
-       document.getElementById('displaycount').innerHTML= 'You have ' + ++count + ' Cookies'; 
+// Multiplication function
+function multiply(number) {
+    return number * 2;
+}
+
+// ** EVENTS  ** //
+
+// Increase score
+btnIncrease.addEventListener("click", () => {
+    count += 1;
+    labelCounter.innerHTML = count;
+    multiplierPriceLabel.innerHTML = count;
+    if (multiplierValue == 0) {
+        multiplierValue = count;
     }
-    
 });
-    
-})();
 
+// Decrease score
+btnDecrease.addEventListener("click", () => {
+    // if the counter is greater than 0
+    if (count > 0) {
+        count -= 1;
+        labelCounter.innerHTML = count;
+        multiplierPriceLabel.innerHTML = count;
+    } else {
+        messageLabel.innerHTML = "You can't go bellow zero!!";
+    }
+});
 
+// Multiply score
+btnMultiplier.addEventListener("click", () => {
+    // check if the multiplier value price before allowing to multiply
 
+    if (count >= multiplierValue) {
+        labelCounter.innerHTML = multiply(count);
+        count -= multiplierValue; // remove multiplier valluee + 3 (step 8)
+        multiplierValue += 3; // the current value
+        multiplierPriceLabel.innerHTML = multiplierValue;
+        labelCounter.innerHTML = count;
+    }
+});
+
+const reset = document.getElementById("reset");
+reset.addEventListener("click", () => {
+    count = 0;
+    counter.textContent = count;
+});
+let autoincrease = () => {
+    setInterval(() => {
+        labelCounter.click();
+    }, 30);
+};
